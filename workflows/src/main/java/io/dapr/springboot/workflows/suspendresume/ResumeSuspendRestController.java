56f9818ada34eb2da8d7f8015563b93ca5fc37da
salaboy <Salaboy@gmail.com>
2025-06-12 10:28:38 +0800

    updating to rc-7

--- workflows/src/main/java/io/dapr/springboot/workflows/suspendresume/ResumeSuspendRestController.java
+++ workflows/src/main/java/io/dapr/springboot/workflows/suspendresume/ResumeSuspendRestController.java
@@ -70,17 +70,17 @@ public class ResumeSuspendRestController {
   }
 
 
-//  @PatchMapping("/suspendresume/suspend")
-//  public void suspendWorkflowInstance(@RequestParam("instanceId") String instanceId) {
-//    logger.info("Suspending Workflow Instance: {}", instanceId);
-//    daprWorkflowClient.suspendWorkflow(instanceId, "suspending workflow instance");
-//  }
-//
-//  @PatchMapping("/suspendresume/resume")
-//  public void resumeWorkflowInstance(@RequestParam("instanceId") String instanceId) {
-//    logger.info("Resuming Workflow Instance: {}", instanceId);
-//    daprWorkflowClient.resumeWorkflow(instanceId, "resuming workflow instance");
-//  }
+  @PatchMapping("/suspendresume/suspend")
+  public void suspendWorkflowInstance(@RequestParam("instanceId") String instanceId) {
+    logger.info("Suspending Workflow Instance: {}", instanceId);
+    daprWorkflowClient.suspendWorkflow(instanceId, "suspending workflow instance");
+  }
+
+  @PatchMapping("/suspendresume/resume")
+  public void resumeWorkflowInstance(@RequestParam("instanceId") String instanceId) {
+    logger.info("Resuming Workflow Instance: {}", instanceId);
+    daprWorkflowClient.resumeWorkflow(instanceId, "resuming workflow instance");
+  }
 
 
 }
